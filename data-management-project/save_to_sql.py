8b1ee5c7bdeb8aaf022e45b2eb93d0509f91d691
nyx3ton <nyx3ton@gmail.com>
2025-11-15 18:11:06 +0100

    adding new data via Faker, adrjusted for gender and country location

--- data-management-project/save_to_sql.py
+++ data-management-project/save_to_sql.py
@@ -83,3 +83,66 @@ engine = create_engine(connection_string)
 table_name = 'HR_Synth_Data'
 df.to_sql(table_name, con=engine, if_exists='append', index=False, schema='dbo')
 #df.info()
+###Library imports and global settings
+from sqlalchemy import create_engine
+import urllib.parse
+
+import numpy as np
+np.seterr(divide = 'ignore', invalid='ignore')
+
+
+import pandas as pd
+pd.options.display.max_columns = None
+pd.set_option('display.max_rows', 100)
+
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import rcParams
+%matplotlib inline
+rcParams['figure.figsize'] = 19, 11
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+import os
+
+from datetime import datetime
+today = datetime.today().strftime('%Y-%m-%d')
+today_odbc = datetime.today()
+
+cwd = os.getcwd()
+
+os.makedirs('./Output', exist_ok=True) 
+os.makedirs('./Input', exist_ok=True)  
+os.makedirs('./model_save/model', exist_ok=True) 
+
+
+### Load CSV data and wrangle some string and int into coategory types
+address = f'./Input/ibm_hr_synthetic_data.csv'
+
+df = pd.read_csv(address)
+display(df)
+
+cat_cols = [
+            'Attrition',
+            'Over18',
+            'BusinessTravel',
+            'Gender',
+            'EducationField',
+            'EnvironmentSatisfaction',
+            'JobInvolvement',
+            'JobLevel',
+            'JobRole',
+            'JobSatisfaction',
+            'PerformanceRating',
+            'RelationshipSatisfaction',
+            'WorkLifeBalance',
+            'MaritalStatus',
+            'OverTime',
+            'Department',
+            'DistanceFromHome',
+            'Education']
+
+df[cat_cols] = df[cat_cols].astype('category')
+display(df)
